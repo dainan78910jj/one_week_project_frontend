67bd8e2efb8e766e1608ae1ac42ad1c700f4c1a8
Dainan Z <dainan.dn@gmail.com>
2025-06-12 01:07:20 +0200

    adjust margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ function Header(props) {
             <div className="px-4 py-5 text-center">
                 <div className="container">
                     <div className="row justify-content-md-center">
-                        <h1 className="display-5 fw-bold text-body-emphasis">Welcome to Dainan's portfolio website!</h1>
+                        <h1 className="display-5 fw-bold text-body-emphasis mt-4">Welcome to Dainan's portfolio website!</h1>
                         <div className="col-col-lg-6 mx-auto-auto">
-                            <p className="lead mb-4">
+                            <p className="lead mt-5 mb-4">
                                 This is a course project trying to practise the skills of bootstrap by building a portolio webpage. I added the framework of React to this project also.
                             </p>
                             <img className="d-block mx-auto" src="https://media.licdn.com/dms/image/v2/C5603AQGVXOFAgjQQJA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1644353207795?e=1755129600&v=beta&t=HOBhxvupYvxuUKAoBJ-_bXYoHG75-aSz3JoILcpTLQQ" />
